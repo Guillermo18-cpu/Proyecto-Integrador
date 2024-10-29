a0f691c48b95b4dcab5cb2093550592a94908d58
Guillermo Sanchez <3126810804juan@gmail.com>
2024-10-29 15:49:20 -0500

    Pantalla Adm

---
+++ src/Administrador.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Administrador {
+    private JPanel panel1;
+    private JButton productoButton;
+}
---
+++ src/Pedido.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Pedido {
+    private JTable table1;
+    private JPanel panel1;
+}
